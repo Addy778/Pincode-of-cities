948cde2d96ce8d725879cf2c96b2360c84b49da2
Adityan Kumar <adityankumar1999@gmail.com>
2020-08-02 06:19:40 +0100

    first commit

---
+++ pincode.py
@@ -0,0 +1,5 @@
+#creating a dictionary to show pincode of the cities
+cities={"Ahmedabad":"380001", "Bangalore":"560002", "Bhubaneswar":"751001", "Chennai":"600001", "Chandigarh":"160017", "Delhi":"110001", "Dehradun":"248001", "Hyderabad":"	500001", "Kolkata":"700001", "Kota":"	324001", "Mumbai":"	400001", "Pune":"411001", "Vizag":"530001"}
+print(" NAMASTE , Please ENTER your City ")
+k=input()
+print("Your PIN CODE is " + str(cities[k]))
